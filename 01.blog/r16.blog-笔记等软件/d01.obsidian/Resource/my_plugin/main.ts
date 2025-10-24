8f9e2c25804aa5a8439747067fc4cb5ecc2e035d
wangbin <2722620236@qq.com>
2025-10-24 21:36:04 +0800

    t

---
+++ 01.blog/r16.blog-笔记等软件/d01.obsidian/Resource/my_plugin/main.ts
@@ -0,0 +1,10 @@
+import { Plugin } from "obsidian";
+
+export default class ExamplePlugin extends Plugin {
+  async onload() {
+    console.log('loading plugin')
+  }
+  async onunload() {
+    console.log('unloading plugin')
+  }
+}
